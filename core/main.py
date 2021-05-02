b714ac7e216ef9a660988160d20be11a153805d4
danil <danil.mirtalibov.27@mail.ru>
2021-05-02 13:05:54 +0300

    Window for db connection done

--- core/main.py
+++ core/main.py
@@ -4,6 +4,5 @@ from gui import graphics
 from db import database
 
 if __name__ == "__main__":
+    graphics.connect_to_db()
     database.db_connect()
-    root = graphics.start()
-    root.mainloop()
