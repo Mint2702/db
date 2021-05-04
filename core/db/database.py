8c7f8054a9be1a12845e573a669fc935e46a8780
danil <danil.mirtalibov.27@mail.ru>
2021-05-04 12:16:56 +0300

    Get for student done

--- core/db/database.py
+++ core/db/database.py
@@ -55,3 +55,16 @@ def get_table_rank(
     result = [[int(record[0]), record[1]] for record in records]
 
     return result
+
+@sql_command
+def get_table_student(
+    cursor,
+) -> list:
+    """ Достает все данные из указанной таблицы """
+
+    command = "SELECT * FROM student"
+    cursor.execute(command)
+    records = cursor.fetchall()
+    result = [[int(record[0]), record[1], record[2], record[3], record[4]] for record in records]
+
+    return result
