65ebbc1b6c7640cbf257984a68319854bc78a5a4
EverlaststingRolich <60112075+EverlaststingRolich@users.noreply.github.com>
2021-05-13 16:42:39 +0300

    student and subject add

--- core/db/post.py
+++ core/db/post.py
@@ -1,16 +1,54 @@
 from .utils import sql_command
 
-#INSERT INTO fruits(name)
-#VALUES('Banana')
-#RETURNING id;
+
+
+
+def get_student_id(cursor, name, surname, passport_num, passport_inn) -> int:
+    """ Возвращаетс id студента """
+    command = f"SELECT id FROM student WHERE first_name='{name}' AND last_name='{surname}' AND passport_num='{passport_num}' AND inn='{passport_inn}'"
+    cursor.execute(command)
+    records = cursor.fetchall()
+    result = [int(record[0]) for record in records]
+    return result[0]
+
 
 @sql_command
-def post_student(cursor, name, surname, date_if_birth, passport_num, passport_date, passport_given, passport_inn, platoon) -> None:
+def post_student(cursor, name, surname, date_if_birth, passport_num, passport_date, passport_given, passport_inn, platoon, address, phone) -> None:
     """ Добавление студентов в таблицу """
     cursor.execute("INSERT INTO student (first_name, last_name, date_of_birth, passport_num,  passport_date,  passport_given, inn, platoon_id) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)", (name, surname, date_if_birth, passport_num, passport_date, passport_given, passport_inn, platoon))
+    student_id = get_student_id(cursor, name, surname, passport_num, passport_inn)
+    cursor.execute(
+        "INSERT INTO student_contacts (id_student, address, phone_number) VALUES(%s, %s, %s)",
+        (student_id, address, phone))
+
+
+def get_rod_id(cursor, rod) -> int:
+    command = f"SELECT id FROM platoon WHERE direction='{rod}'"
+    cursor.execute(command)
+    records = cursor.fetchall()
+    result = [int(record[0]) for record in records]
+    return result[0]
+
+
+def get_subject_id(cursor, name, year, semestr) -> int:
+    command = f"SELECT id FROM subject WHERE name='{name}' AND year_of_study='{year}' AND semester_of_study='{semestr}'"
+    cursor.execute(command)
+    records = cursor.fetchall()
+    result = [int(record[0]) for record in records]
+    return result[0]
 
 
 @sql_command
-def post_subject() -> None:
+def post_subject(cursor, name, year, semestr, rod) -> None:
     """ Добавление предметов """
-    print('test')
+    rod_id = get_rod_id(cursor, rod)
+    cursor.execute(
+        "INSERT INTO subject (name, year_of_study, semester_of_study) VALUES(%s, %s, %s)",
+        (name, year, semestr))
+
+    subject_id = get_subject_id(cursor, name, year, semestr)
+    cursor.execute(
+        "INSERT INTO subject_of_platoon (id_platoon, id_subject) VALUES(%s, %s)",
+        (rod_id, subject_id))
+
+
--- core/gui/windows/add_data.py
+++ core/gui/windows/add_data.py
@@ -40,7 +40,7 @@ class AddDataWindow(Frame):
 
     def place_add_frame(self) -> None:
         """Создание фрейма для добавления"""
-        self.add_frame = Frame(self, width=600, height=400)
+        self.add_frame = Frame(self, width=800, height=400)
         self.add_frame.place(x=500, y=50)
 
     def place_add_subjects_forms(self) -> None:
@@ -282,6 +282,21 @@ class AddDataWindow(Frame):
         )
         student_label_8.place(x=5, y=310)
 
+        student_label_9 = Label(
+            self.add_frame,
+            text=""
+                 "Адрес: ",
+            font=("Arial Bold", 14),
+        )
+        student_label_9.place(x=400, y=100)
+
+        student_label_10 = Label(
+            self.add_frame,
+            text="Номер телефона: ",
+            font=("Arial Bold", 14),
+        )
+        student_label_10.place(x=400, y=130)
+
         self.student_name = StringVar()
         self.student_surname = StringVar()
         self.student_date_of_birth = StringVar()
@@ -289,6 +304,8 @@ class AddDataWindow(Frame):
         self.student_passport_date = StringVar()
         self.student_passport_given = StringVar()
         self.student_passport_inn = StringVar()
+        self.student_address = StringVar()
+        self.student_phone = StringVar()
 
         student_name_entry = Entry(self.add_frame, textvariable=self.student_name)
         student_name_entry.place(x=230, y=100)
@@ -317,6 +334,16 @@ class AddDataWindow(Frame):
 
         self.student_platoon = ttk.Combobox(self.add_frame, values=[1, 2, 3], width=2)
         self.student_platoon.place(x=230, y=310)
+
+        student_address_entry = Entry(
+            self.add_frame, textvariable=self.student_address
+        )
+        student_address_entry.place(x=580, y=100)
+
+        student_phone_entry = Entry(
+            self.add_frame, textvariable=self.student_phone
+        )
+        student_phone_entry.place(x=580, y=130)
 
     def place_add_equipment_forms(self) -> None:
         """Создание форм для добавления оборудования"""
@@ -412,10 +439,7 @@ class AddDataWindow(Frame):
         from db.post import post_student, post_subject
 
         if self.add_status == "Добавление предмета":
-            print(self.subject_name.get())
-            print(self.subject_year.get())
-            print(self.subject_semestr.get())
-            print(self.subject_rod.get())
+            post_subject(self.subject_name.get(), self.subject_year.get(), self.subject_semestr.get(), self.subject_rod.get())
 
         elif self.add_status == "Добавление преподавателя":
             print("Преподаватель добавлен")
@@ -430,6 +454,8 @@ class AddDataWindow(Frame):
                 self.student_passport_given.get(),
                 self.student_passport_inn.get(),
                 self.student_platoon.get(),
+                self.student_address.get(),
+                self.student_phone.get()
             )
 
         elif self.add_status == "Добавление оборудования":
